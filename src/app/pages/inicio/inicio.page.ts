a9017831a6309a2811e728f3969f4c803fdbaf02
Ruben Dario Chalco Campos <nebur.dario@gmail.com>
2020-05-03 22:44:40 -0400

    hasta infinite scroll

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,69 +1,74 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-    selector: 'app-inicio',
-    templateUrl: './inicio.page.html',
-    styleUrls: ['./inicio.page.scss'],
+	selector: 'app-inicio',
+	templateUrl: './inicio.page.html',
+	styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-    pagesList: PagesLIst[] = [
-        {
-            name: 'alert',
-            icon: 'alarm',
-            redirectTo: '/alert'
-        },
-        {
-            name: 'action-sheet',
-            icon: 'warning',
-            redirectTo: '/action-sheet'
-        },
-        {
-            name: 'avatar',
-            icon: 'person',
-            redirectTo: '/avatar'
-        },
-        {
-            name: 'botones',
-            icon: 'square',
-            redirectTo: '/botones'
-        },
-        {
-            name: 'card',
-            icon: 'card',
-            redirectTo: '/card'
-        },
-        {
-            name: 'check',
-            icon: 'checkbox',
-            redirectTo: '/check'
-        },
-        {
-            name: 'dateTime',
-            icon: 'calendar',
-            redirectTo: '/date-time'
-        },
-        {
-            name: 'fabe',
-            icon: 'car',
-            redirectTo: '/fabe'
-        },
-	    {
-		    name: 'grid',
-		    icon: 'grid',
-		    redirectTo: '/grid'
-	    }
-    ];
+	pagesList: PagesLIst[] = [
+		{
+			name: 'alert',
+			icon: 'alarm',
+			redirectTo: '/alert'
+		},
+		{
+			name: 'action-sheet',
+			icon: 'warning',
+			redirectTo: '/action-sheet'
+		},
+		{
+			name: 'avatar',
+			icon: 'person',
+			redirectTo: '/avatar'
+		},
+		{
+			name: 'botones',
+			icon: 'square',
+			redirectTo: '/botones'
+		},
+		{
+			name: 'card',
+			icon: 'card',
+			redirectTo: '/card'
+		},
+		{
+			name: 'check',
+			icon: 'checkbox',
+			redirectTo: '/check'
+		},
+		{
+			name: 'dateTime',
+			icon: 'calendar',
+			redirectTo: '/date-time'
+		},
+		{
+			name: 'fabe',
+			icon: 'car',
+			redirectTo: '/fabe'
+		},
+		{
+			name: 'grid',
+			icon: 'grid',
+			redirectTo: '/grid'
+		},
+		{
+			name: 'infinite scroll',
+			icon: 'infinite',
+			redirectTo: '/infinite-scroll'
+		}
+	];
 
-    constructor() {
-    }
+	constructor() {
+	}
 
-    ngOnInit() {
-    }
+	ngOnInit() {
+	}
 
 }
 
 export interface PagesLIst {
-    name: string;
-    icon: string;
-    redirectTo: string;
+	name: string;
+	icon: string;
+	redirectTo: string;
 }
